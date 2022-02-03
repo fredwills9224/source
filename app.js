d819f1e07a13d7f710a23895e385d17f6ff0b823
fredwills9224 <willsf9224@gmail.com>
2022-02-03 15:14:50 -0500

    app.post('users') ts pass 200 default res

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const app = express();
 
+// handling the [app.post] [req]uest to ['/api/1.0/users'] endpoint
+    app.post('/api/1.0/users', (req, res)=>{
+        return res.send();
+    });
+// handling the [app.post] [req]uest to ['/api/1.0/users'] endpoint
+
 module.exports = app;
