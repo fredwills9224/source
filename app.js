28476dce4157c1fea1f7c83f4ac102feda3f0936
fredwills9224 <willsf9224@gmail.com>
2022-02-03 15:39:15 -0500

    app.post('users') success message ts pass

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 
 // handling the [app.post] [req]uest to ['/api/1.0/users'] endpoint
     app.post('/api/1.0/users', (req, res)=>{
-        return res.send();
+        return res.send({message: 'User created'});
     });
 // handling the [app.post] [req]uest to ['/api/1.0/users'] endpoint
 
