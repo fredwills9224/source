1f88b66e8e76b219df7e4cdd88a699a8bfc7068a
fredwills9224 <willsf9224@gmail.com>
2022-02-05 14:44:22 -0500

    usrnm notNull ts p w/ em

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -7,7 +7,13 @@ const UserService = require('./UserService');
         
         const user = req.body;
         if(user.username === null){
-            return res.status(400).send({ validationErrors: {} });
+            return res.status(400).send({
+
+                validationErrors: {
+                    username: 'Username cannot be null'
+                }
+                 
+            });
         }
         await UserService.save(req.body);
         return res.send({ message: 'User created' });
