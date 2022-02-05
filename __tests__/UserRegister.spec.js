1701150c9ec915ff5ea07b64e4b2637e7ec8bda6
fredwills9224 <willsf9224@gmail.com>
2022-02-05 12:03:46 -0500

    user registration tss refactor 1

--- __tests__/UserRegister.spec.js
+++ __tests__/UserRegister.spec.js
@@ -21,58 +21,37 @@ describe('User Registration', ()=>{
 
         });
     };
-    it('returns 200 OK when signup request is valid', (done)=>{
-        postValidUser().then((response)=>{
-            
-            expect(response.status).toBe(200);
-            done();
-
-        });
+    it('returns 200 OK when signup request is valid', async ()=>{
+        const response = await postValidUser();
+        expect(response.status).toBe(200);
     }); 
-    it('returns success message when signup request is valid', (done)=>{
-        postValidUser().then((response)=>{
-            
-            expect(response.body.message).toBe('User created');
-            done();
-
-        });
+    it('returns success message when signup request is valid', async ()=>{
+        const response = await postValidUser();
+        expect(response.body.message).toBe('User created');
     });
-    it('saves the user to database', (done)=>{
-        postValidUser().then(()=>{
-
-            User.findAll().then((userList)=>{
-                expect(userList.length).toBe(1);
-                done();
-            });
-            
-        });
-    });
-    it('saves the username and email to database', (done)=>{
+    it('saves the user to database', async ()=> {
         
-        postValidUser().then(()=>{
-            User.findAll().then((userList)=>{
+        await postValidUser();
+        const userList = await User.findAll();
+        expect(userList.length).toBe(1);
+            
+    });
+    it('saves the username and email to database', async ()=>{
+        
+        await postValidUser();
+        const userList = await User.findAll();        
+        const savedUser = userList[0];
+        expect(savedUser.username).toBe('user1');
+        expect(savedUser.email).toBe('user1@mail.com');
                 
-                const savedUser = userList[0];
-                expect(savedUser.username).toBe('user1');
-                expect(savedUser.email).toBe('user1@mail.com');
-                done();
-
-            });
-        });
-    
     });
-    it('it hashes the password in database', (done)=>{
+    it('it hashes the password in database', async ()=>{
         
-        postValidUser().then(()=>{
-            User.findAll().then((userList)=>{
-
-                const savedUser = userList[0];
-                expect(savedUser.password).not.toBe('user1password');
-                done();
-
-            });
-        });
-
+        await postValidUser();
+        const userList = await User.findAll();
+        const savedUser = userList[0];
+        expect(savedUser.password).not.toBe('user1password');
+                
     });
 
 });
