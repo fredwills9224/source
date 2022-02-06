f905159740299219b04e42da991b88bc44d3d88b
fredwills9224 <willsf9224@gmail.com>
2022-02-06 17:32:35 -0500

    internationalization w/tr ts p

--- src/app.js
+++ src/app.js
@@ -1,15 +1,57 @@
 const express = require('express');
 const UserRouter = require('./user/UserRouter');
+const i18next = require('i18next');
+const Backend = require('i18next-fs-backend');
+const middlewre = require('i18next-http-middleware');
 
-// initializing [app]'s server
+i18next
+    .use(Backend)
+    .use(middlewre.LanguageDetector)
+    .init({
+
+        fallbackLng: 'en',
+        lng: 'en',
+        ns: ['translation'],
+        defaultNS: 'translation',
+        backend:{
+            loadPath: './locales/{{lng}}/{{ns}}.json'
+        },
+        detection:{
+            lookupHeader: 'accept-language'
+        }
+
+    })
+;
+
+// [app]'s server
     const app = express();
-// initializing [app]'s server
-// initializing json body parser
+// [app]'s server
+// internationalization
+    app.use(middlewre.handle(i18next));
+// internationalization
+// json body parser
     app.use(express.json());
-// initializing json body parser
-// connecting [router]
+// json body parser
+// [router]
     app.use(UserRouter);
-// connecting [router]
+// [router]
 console.log('env: ' + process.env.NODE_ENV);
 
 module.exports = app;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
