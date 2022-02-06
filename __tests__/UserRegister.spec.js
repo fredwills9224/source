cf9688c5719d0764024c53bcdcf7a686df0cbf44
fredwills9224 <willsf9224@gmail.com>
2022-02-06 01:12:27 -0500

    password size ts f

--- __tests__/UserRegister.spec.js
+++ __tests__/UserRegister.spec.js
@@ -98,7 +98,10 @@ describe('User Registration', ()=>{
                     ${'username'}  |  ${'a'.repeat(33)}  | ${'Must have min 4 and max 32 characters'}
                     ${'email'}     |  ${null}            | ${'E-mail cannot be null'}
                     ${'email'}     |  ${'mail.com'}      | ${'E-mail is not valid'}
+                    ${'email'}     |  ${'user.mail.com'} | ${'E-mail is not valid'}
+                    ${'email'}     |  ${'user@mail'}     | ${'E-mail is not valid'}
                     ${'password'}  |  ${null}            | ${'Password cannot be null'} 
+                    ${'password'}  |  ${'P4ssw'}         | ${'Password must be at least 6 characters'} 
                 `('returns  $expectedMessage when $field is $value', async ({ field, expectedMessage, value })=>{
 
                 const user = {
