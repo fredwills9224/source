21b919c8f689b8ee4d2d94d306ee61231165cbea
fredwills9224 <willsf9224@gmail.com>
2022-02-07 08:48:28 -0500

    dsu n db emf ts p 502 ts f emf + emf tr ts f

--- src/user/UserService.js
+++ src/user/UserService.js
@@ -2,6 +2,7 @@ const User = require('./User');
 const bcrypt = require('bcrypt');
 const crypto = require('crypto');
 const EmailService = require('../email/EmailService');
+const sequelize = require('../config/database');
 
 const generateToken = (length)=>{
     return crypto.randomBytes(length).toString('hex').substring(0, length);
@@ -12,8 +13,14 @@ const generateToken = (length)=>{
         const { username, email, password } = body;
         const hashedPassword = await bcrypt.hash(password, 10);
         const user = { username, email, password: hashedPassword, activationToken: generateToken(16) };
+        const transaction = await sequelize.transaction();
         await User.create(user);
-        await EmailService.sendAccountActivation(email, user.activationToken);
+        try{
+            await EmailService.sendAccountActivation(email, user.activationToken);
+            await transaction.commit();            
+        }catch (err){
+            await transaction.rollback();
+        }
     
     };
 // [save]s [user] with [hashedPassword]
