9ea13fb748cb8dc0e128c7a4857b1259e60d186c
fredwills9224 <willsf9224@gmail.com>
2022-02-06 21:17:35 -0500

    usr svd w/ inactive prop ts p

--- src/user/User.js
+++ src/user/User.js
@@ -4,21 +4,27 @@ const Model = Sequelize.Model;
 
 class User extends Model{}
 
-User.init(
-    {
-        username:{
-            type: Sequelize.STRING
-        },
-        email:{
-            type: Sequelize.STRING
-        },
-        password:{
-            type: Sequelize.STRING
-        }
+User.init({
+
+    username:{
+        type: Sequelize.STRING
+    },
+    email:{
+        type: Sequelize.STRING
+    },
+    password:{
+        type: Sequelize.STRING
+    },
+    inactive:{
+        type: Sequelize.BOOLEAN,
+        defaultValue: true
+    }
+
     },{
         sequelize,
         modelName: 'user'
-    }
-);
+    })
+
+;
 
 module.exports = User;
