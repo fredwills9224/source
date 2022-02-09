22a90b05f4e1af972af9252bb396728fa9cf5dc0
fredwills9224 <willsf9224@gmail.com>
2022-02-09 18:44:30 -0500

    rtrns ptSmvE n r whn vf ts p

--- src/error/ErrorHandler.js
+++ src/error/ErrorHandler.js
@@ -7,6 +7,11 @@ module.exports = ((err, req, res, next)=>{
         validationErrors = {};
         errors.forEach( (error)=> (validationErrors[error.param] = req.t(error.msg)) );
     }
-    res.status(status).send({ message: req.t(message), validationErrors });
+    res.status(status).send({ 
+        path: '',
+        timestamp:'',
+        message: req.t(message),
+        validationErrors
+    });
 
 });
