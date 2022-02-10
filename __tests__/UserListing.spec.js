42bf99dd8ea4cd4ebd28c51b321749235a5c9e3a
fredwills9224 <willsf9224@gmail.com>
2022-02-09 22:35:47 -0500

    rtrns pgO as rb ts f

--- __tests__/UserListing.spec.js
+++ __tests__/UserListing.spec.js
@@ -7,5 +7,16 @@ describe('Listing Users', ()=>{
         const response = await request(app).get('/api/1.0/users');
         expect(response.status).toBe(200);    
     });
+    it('returns page object as response body', async()=>{
+
+        const response = await request(app).get('/api/1.0/users');
+        expect(response.body).toEqual({
+            content:[],
+            page: 0,
+            size: 10,
+            totalPages:0
+        });
+
+    });
 
 });
