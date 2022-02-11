9b1c562062ab5c16bd4fe6ecb03e9c824670d6fd
fredwills9224 <willsf9224@gmail.com>
2022-02-11 09:03:02 -0500

    rtns 401 whnusrdne ts p

--- src/auth/AuthenticationRouter.js
+++ src/auth/AuthenticationRouter.js
@@ -6,6 +6,9 @@ router.post('/api/1.0/auth', async (req, res)=>{
     
     const { email } = req.body;
     const user = await UserService.findByEmail(email);
+    if(!user){
+        return res.status(401).send();
+    }
     res.send({
         id: user.id,
         username: user.username
