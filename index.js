d74cac630f543c4de600b1c4d6e6cdc01161fa90
fredwills9224 <willsf9224@gmail.com>
2022-02-11 18:59:50 -0500

    usrpwndb

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const app = require('./src/app');
 const sequelize = require('./src/config/database');
 const User = require('./src/user/User');
+const bcrypt = require('bcrypt');
 
 const addUsers = async (activeUserCount, inactiveUserCount=0)=>{
 
+    const hashedPassword = await bcrypt.hash('User1password', 10);
     for(let i=0; i<activeUserCount+inactiveUserCount; i++){
         await User.create({
             username: `user${i +1}`,
             email: `user${i +1}@mail.com`,
-            inactive: i>= activeUserCount
+            inactive: i>= activeUserCount,
+            password: hashedPassword
         });
     }
 
