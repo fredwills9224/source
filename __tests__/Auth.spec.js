7118f28dfa458659c2ba5d0cbbc1cb98c4d41172
fredwills9224 <willsf9224@gmail.com>
2022-02-11 06:35:16 -0500

    rtns 200 whn cac ts f

---
+++ __tests__/Auth.spec.js
@@ -0,0 +1,45 @@
+const request = require('supertest');
+const app = require('../src/app');
+const User = require('../src/user/User');
+const sequelize = require('../src/config/database');
+const bcrypt = require('bcrypt');
+
+beforeAll(async ()=>{
+    await sequelize.sync();
+});
+beforeEach(async()=>{
+    await User.destroy({ truncate: true });
+});
+
+const addUsers = async ()=>{
+
+    const user = { 
+        username: 'user1',
+        email: 'user1@mail.com',
+        password: 'User1password',
+        inactive: false
+    };
+    const hashedPassword = await bcrypt.hash(user.password, 10);
+    user.password = hashedPassword;
+    await User.create(user);
+
+};
+const postAuthentication = async (credentials)=>{
+
+    return await request(app).post('/api/1.0/auth').send(credentials);
+
+};
+describe('Authentication', ()=>{
+
+    it('returns 200 when credentials are correct', async ()=>{
+
+        await addUsers();
+        const response = await postAuthentication({ 
+            email: 'user1@mail.com', 
+            password: 'User1password' 
+        });
+        expect(response.status).toBe(200);
+
+    });
+
+});
