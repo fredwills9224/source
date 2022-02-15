e4c0920bfa9fb00b1d2acb8be92be003143b1c63
fredwills9224 <willsf9224@gmail.com>
2022-02-14 20:55:21 -0500

    rtns 200 ok whn aprr isfknwnem ts f

--- __tests__/PasswordReset.spec.js
+++ __tests__/PasswordReset.spec.js
@@ -1,8 +1,31 @@
 const request = require('supertest');
 const app = require('../src/app');
+const User = require('../src/user/User');
+const sequelize = require('../src/config/database');
+const bcrypt = require('bcrypt');
 const en = require('../locales/en/translation.json');
 const tr = require('../locales/tr/translation.json');
 
+beforeAll(async ()=>{
+    await sequelize.sync();
+});
+beforeEach(async ()=>{
+    await User.destroy({ truncate: { cascade: true } });
+});
+
+const activeUser = { 
+    username: 'user1',
+    email: 'user1@mail.com',
+    password: 'User1password',
+    inactive: false
+};
+const addUser = async (user = {...activeUser})=>{
+
+    const hashedPassword = await bcrypt.hash(user.password, 10);
+    user.password = hashedPassword;
+    return await User.create(user);
+
+};
 const postPasswordReset = (email = 'user1@mail.com', options = {})=>{
 
     const agent = request(app).post('/api/1.0/password-reset');
@@ -14,37 +37,52 @@ const postPasswordReset = (email = 'user1@mail.com', options = {})=>{
 };
 describe('Password Reset Request', ()=>{
 
-    it('returns 404 when a password reset request is sent for unkown e-mail', async ()=>{
+    // in[validUser]
 
-        const response = await postPasswordReset();
-        expect(response.status).toBe(404);
-        
-    });
-    it.each`
-        language | message
-        ${'tr'}  | ${tr.email_not_inuse}
-        ${'en'}  | ${en.email_not_inuse}
-        `('returns error body with $message for unknown email for password reset request when language is $language',
-        async ({ language, message })=>{ 
+        it('returns 404 when a password reset request is sent for unkown e-mail', async ()=>{
 
-        const nowInMillis = new Date().getTime();
-        const response = await postPasswordReset('user1@mail.com', {language: language});
-        expect(response.body.path).toBe('/api/1.0/password-reset');
-        expect(response.body.timestamp).toBeGreaterThan(nowInMillis);
-        expect(response.body.message).toBe(message);
+            const response = await postPasswordReset();
+            expect(response.status).toBe(404);
 
-    });
-    it.each`
-        language | message
-        ${'tr'}  | ${tr.email_invalid}
-        ${'en'}  | ${en.email_invalid}
-        `('returns 400 with validation error response having $message when request does not have valid email and language is $language',
-        async ({ language, message })=>{ 
+        });
+        it.each`
+            language | message
+            ${'tr'}  | ${tr.email_not_inuse}
+            ${'en'}  | ${en.email_not_inuse}
+            `('returns error body with $message for unknown email for password reset request when language is $language',
+            async ({ language, message })=>{
 
-        const response = await postPasswordReset(null, {language: language});
-        expect(response.body.validationErrors.email).toBe(message);
-        expect(response.status).toBe(400);
+            const nowInMillis = new Date().getTime();
+            const response = await postPasswordReset('user1@mail.com', {language: language});
+            expect(response.body.path).toBe('/api/1.0/password-reset');
+            expect(response.body.timestamp).toBeGreaterThan(nowInMillis);
+            expect(response.body.message).toBe(message);
 
-    });
+        });
+        it.each`
+            language | message
+            ${'tr'}  | ${tr.email_invalid}
+            ${'en'}  | ${en.email_invalid}
+            `('returns 400 with validation error response having $message when request does not have valid email and language is $language',
+            async ({ language, message })=>{
+
+            const response = await postPasswordReset(null, {language: language});
+            expect(response.body.validationErrors.email).toBe(message);
+            expect(response.status).toBe(400);
+
+        });
+
+    // in[validUser]
+    // [validUser]
+
+        it('returns 200 ok when a password reset request is sent for known email', async ()=>{
+
+            const user = await addUser();
+            const response = await postPasswordReset(user.email);
+            expect(response.status).toBe(200);
+
+        });
+
+    // [validUser]
 
 });
