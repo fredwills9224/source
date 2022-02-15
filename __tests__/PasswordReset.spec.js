b058eed672b15a8d4101015ff937d864a66560f0
fredwills9224 <willsf9224@gmail.com>
2022-02-14 19:49:57 -0500

    postPasswordReset rfctr atsp

--- __tests__/PasswordReset.spec.js
+++ __tests__/PasswordReset.spec.js
@@ -3,14 +3,20 @@ const app = require('../src/app');
 const en = require('../locales/en/translation.json');
 const tr = require('../locales/tr/translation.json');
 
+const postPasswordReset = (email = 'user1@mail.com', options = {})=>{
+
+    const agent = request(app).post('/api/1.0/password-reset');
+    if(options.language){
+        agent.set('Accept-Language', options.language);
+    }
+    return agent.send({email: email});
+
+};
 describe('Password Reset Request', ()=>{
 
     it('returns 404 when a password reset request is sent for unkown e-mail', async ()=>{
 
-        const response = await request(app)
-            .post('/api/1.0/password-reset')
-            .send({email: 'user1@mail.com'})
-        ;
+        const response = await postPasswordReset();
         expect(response.status).toBe(404);
         
     });
@@ -22,11 +28,7 @@ describe('Password Reset Request', ()=>{
         async ({ language, message })=>{ 
 
         const nowInMillis = new Date().getTime();
-        const response = await request(app)
-            .post('/api/1.0/password-reset')
-            .set('Accept-Language', language)
-            .send({email: 'user1@mail.com'})
-        ;
+        const response = await postPasswordReset('user1@mail.com', {language: language});
         expect(response.body.path).toBe('/api/1.0/password-reset');
         expect(response.body.timestamp).toBeGreaterThan(nowInMillis);
         expect(response.body.message).toBe(message);
