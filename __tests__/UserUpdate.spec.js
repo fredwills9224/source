8cc8e126424aac191bae520ba3462e67fdd946df
fredwills9224 <willsf9224@gmail.com>
2022-02-16 18:16:43 -0500

    susrimg tupldfldr+sflnmiusr whn upd8himg rfctr2 tp

--- __tests__/UserUpdate.spec.js
+++ __tests__/UserUpdate.spec.js
@@ -15,6 +15,16 @@ beforeAll(async ()=>{
 beforeEach(async ()=>{
     await User.destroy({ truncate: { cascade: true } });
 });
+afterAll(()=>{
+
+    const { uploadDir, profileDir } = config;
+    const profileDirectory = path.join('.', uploadDir, profileDir);
+    const files = fs.readdirSync(profileDirectory);
+    for(const file of files){
+        fs.unlinkSync(path.join(profileDirectory, file));
+    }
+
+});
 
 const activeUser = { 
     username: 'user1',
