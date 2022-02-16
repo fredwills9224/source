8d749459455e401cf69c9b022e868c4741b3cd04
fredwills9224 <willsf9224@gmail.com>
2022-02-16 10:55:15 -0500

    cr8s prflfldr undr upldfldr ts p

--- src/file/FileService.js
+++ src/file/FileService.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 
 const createFolders = ()=>{
 
@@ -6,7 +7,11 @@ const createFolders = ()=>{
     if(!fs.existsSync(uploadDir)){
         fs.mkdirSync('upload');
     }
-    
+    const profileFolder = path.join('.', 'upload', 'profile');
+    if(!fs.existsSync(profileFolder)){
+        fs.mkdirSync(profileFolder);
+    }
+
 };
 
 module.exports = { createFolders };
