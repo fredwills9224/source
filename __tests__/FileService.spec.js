1c3dbd83df6fcec9c59978e8646c5632214d765b
fredwills9224 <willsf9224@gmail.com>
2022-02-16 10:46:51 -0500

    cr8s prflfldr undr upldfldr ts f

--- __tests__/FileService.spec.js
+++ __tests__/FileService.spec.js
@@ -1,5 +1,6 @@
 const FileService = require('../src/file/FileService');
 const fs = require('fs');
+const path = require('path');
 
 describe('createFolders', ()=>{
 
@@ -8,6 +9,13 @@ describe('createFolders', ()=>{
         FileService.createFolders();
         const folderName = 'upload';
         expect(fs.existsSync(folderName)).toBe(true);
+
+    });
+    it('creates profile folder under upload folder', ()=>{
+
+        FileService.createFolders();
+        const profileFolder = path.join('.', 'upload', 'profile');
+        expect(fs.existsSync(profileFolder)).toBe(true);
 
     });
 
