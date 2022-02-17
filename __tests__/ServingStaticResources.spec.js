3e57cffd4e4a7cc983b8a950e7b95efaeda22c44
fredwills9224 <willsf9224@gmail.com>
2022-02-16 19:37:52 -0500

    r200 whn fxst ts f

--- __tests__/ServingStaticResources.spec.js
+++ __tests__/ServingStaticResources.spec.js
@@ -1,12 +1,27 @@
 const request = require('supertest');
 const app = require('../src/app');
+const fs = require('fs');
+const path = require('path');
+const config = require('config');
 
+const { uploadDir, profileDir } = config;
+const profileFolder = path.join('.', uploadDir, profileDir);
 describe('Profile Images', ()=>{
 
     it('returns 404 when file not found', async()=>{
 
         const response = await request(app).get('/images/123456');
         expect(response.status).toBe(404);
+
+    });
+    it('returns 200 when file exist', async ()=>{
+
+        const filePath = path.join('.', '__tests__', 'resources', 'test-png.png');
+        const storedFileName = 'test-file';
+        const targetPath = path.join(profileFolder, storedFileName);
+        fs.copyFileSync(filePath, targetPath);
+        const response = await request(app).get('/images/' + storedFileName);
+        expect(response.status).toBe(200);
 
     });
 
