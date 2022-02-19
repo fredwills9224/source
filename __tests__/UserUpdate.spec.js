cd2406fc4a467c34adbe04e34ef04232a6e22b63
fredwills9224 <willsf9224@gmail.com>
2022-02-19 01:49:00 -0500

    rtns msg whn upldnflaimg whn lil ts f

--- __tests__/UserUpdate.spec.js
+++ __tests__/UserUpdate.spec.js
@@ -496,5 +496,36 @@ describe('User Update', ()=>{
         expect(response.status).toBe(status);
 
     });
+    it.each`
+            file              | language | message
+            ${'test-gif.gif'} | ${'tr'}  | ${tr.unsupported_image_file}
+            ${'test-gif.gif'} | ${'en'}  | ${en.unsupported_image_file}
+            ${'test-pdf.pdf'} | ${'tr'}  | ${tr.unsupported_image_file}
+            ${'test-pdf.pdf'} | ${'en'}  | ${en.unsupported_image_file}
+            ${'test-txt.txt'} | ${'tr'}  | ${tr.unsupported_image_file}
+            ${'test-txt.txt'} | ${'en'}  | ${en.unsupported_image_file}
+        `('returns $message when uploading $file as image when language is $language', 
+        async ({message, file, language})=>{
+
+        const fileInBase64 = readFileAsBase64(file);
+        const savedUser = await addUser();
+        const updateBody = { username: 'user1-updated', image: fileInBase64 };
+        const response = await putUser(
+            
+            savedUser.id,
+            updateBody,
+            { 
+                auth: 
+                {
+                    email: savedUser.email,
+                    password: 'User1password'
+                },
+                language 
+            }
+        
+        );
+        expect(response.body.validationErrors.image).toBe(message);
+    
+    });
 
 });
