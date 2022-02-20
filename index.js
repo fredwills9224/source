71436fc55cb978324ad8c0340bd6c3942b25771d
fredwills9224 <willsf9224@gmail.com>
2022-02-20 05:43:50 -0500

    logging app version

--- index.js
+++ index.js
@@ -13,4 +13,4 @@ logger.verbose('verbose');
 logger.debug('debug');
 logger.silly('silly');
 
-app.listen(3000, ()=> logger.info('app is running!!!!'));
+app.listen(3000, ()=> logger.info('app is running. version: ' + process.env.npm_package_version));
