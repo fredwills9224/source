0ac59f6b88ef427c06eccf8003a83f74c4bfef65
fredwills9224 <willsf9224@gmail.com>
2022-02-28 18:03:16 -0500

    running tests with postgresql

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,8 @@
 const Sequelize = require('sequelize');
 const config = require('config');
+// parsing timestamp for postgres
+    require('pg').defaults.parseInt8 = true;
+// parsing timestamp for postgres
 
 const dbConfig = config.get('database');
 const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
