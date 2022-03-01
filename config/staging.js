cfd5016aaf25ad1f4e0bd3351a73e7207cf9643e
fredwills9224 <willsf9224@gmail.com>
2022-02-28 19:27:39 -0500

    before running tests with postgresql

--- config/staging.js
+++ config/staging.js
@@ -2,10 +2,10 @@ module.exports = {
     
     database:{
         database: 'hoaxify',
-        username: 'postgres',
-        password: 'Unity2014!!@@',
-        dialect: 'postgres',
-        host: 'localhost',
+        username: 'my-db-user',
+        password: 'db-p4ss',
+        dialect: 'sqlite',
+        storage: './staging.sqlite',
         logging: false
     },
     mail: {
