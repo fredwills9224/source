63a730a9a7410873f650610880b4e9e38f174b51
fredwills9224 <willsf9224@gmail.com>
2022-03-03 19:53:43 -0500

    scheduled Service for Unused Attachments

--- index.js
+++ index.js
@@ -2,15 +2,10 @@ const app = require('./src/app');
 const sequelize = require('./src/config/database');
 const TokenService = require('./src/auth/TokenService');
 const logger = require('./src/shared/logger');
+const FileService = require('./src/file/FileService');
 
 sequelize.sync();
 TokenService.scheduleCleanup();
-
-logger.error('error');
-logger.warn('warn');
-logger.info('info');
-logger.verbose('verbose');
-logger.debug('debug');
-logger.silly('silly');
+FileService.removeUnusedAttachments();
 
 app.listen(process.env.PORT || 3000, ()=> logger.info('app is running. version: ' + process.env.npm_package_version));
